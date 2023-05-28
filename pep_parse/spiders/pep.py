20466e86f2d44c4a188ec58c05aa26ed67bfab84
Карина Егорова <karishka.eo@yandex.ru>
2023-05-28 18:47:21 +0300

    review

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,30 @@
+import re
+
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        index = response.xpath('//*[@id="numerical-index"]').css("tbody")
+        all_hrefs = index.css("a::attr(href)").getall()
+        for href in all_hrefs:
+            yield response.follow(href, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        pep = response.css("section[id='pep-content']")
+        pattern = re.compile(r"^PEP\s(?P<number>\d+)[\s–]+(?P<name>.*)")
+        h1_tag = pattern.search(pep.css("h1::text").get())
+        if h1_tag:
+            number, name = h1_tag.group("number", "name")
+        context = {
+            'number': number,
+            'name': name,
+            'status': pep.css("dt:contains('Status') + dd::text").get()
+        }
+        yield PepParseItem(context)
